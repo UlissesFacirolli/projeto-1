f33a285ff52d21de59fbd61602d0752e64c2293f
UlissesFacirolli <00001106690096sp@al.educacao.sp.gov.br>
2024-07-31 14:54:22 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+   background("white");
+}
+
+function draw() {
+  
+  
+  fill ("red");
+  stroke ("lightblue");
+  
+  if (mouseIsPressed); {
+  circle (mouseX,mouseY,30);
+  }
+}
